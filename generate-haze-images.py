3c378b4d3893d3bf29869248fdcbb4486d853756
Arturo Suárez <manuel.suarez@cimat.mx>
2022-11-20 12:51:50 -0600

    Generación y almacenamiento de las imágenes generadas

--- generate-haze-images.py
+++ generate-haze-images.py
@@ -48,3 +48,31 @@ for dirname in base_dir:
         os.mkdir(os.path.join(root_folder,img_dir,dirname,'haze','beta05'))
         os.mkdir(os.path.join(root_folder,img_dir,dirname,'haze','beta15'))
         os.mkdir(os.path.join(root_folder,img_dir,dirname,'haze','beta25'))
+
+# Generación de imágenes sintéticas
+
+# Tres valores para el coeficiente de difusión
+betas = [0.5, 1.5, 2.5] # Scattering coefficient
+dirbetas = ['beta05', 'beta15', 'beta25']
+A = 1      # Atmospheric light
+for beta, dirbeta in zip(betas, dirbetas):
+    for basedir in base_dir:
+        for i, filename in enumerate(os.listdir(os.path.join(root_folder,img_dir,basedir,clean_dir))):
+            # Image
+            img = np.array(Image.open(os.path.join(root_folder,img_dir,basedir,clean_dir,filename)))
+            # Depth map
+            depthname = filename.split(".")[0]+".png"
+            depth = np.array(Image.open(os.path.join(root_folder,img_dir,basedir,depth_dir,depthname)))
+            # Verificamos coincidencia de dimensiones entre la imagen y su mapa de profundidad (recortamos si es necesario)
+            if img.shape[0] != depth.shape[0] or img.shape[1] != depth.shape[1]:
+                img = img[:depth.shape[0],:depth.shape[1],:]
+            # Hazing
+            tx = np.exp(-beta * depth / 255) # Transmission map
+            haze = np.zeros_like(img, dtype=np.uint8)
+            haze[:,:,0] = img[:,:,0]*tx + A*255*(1-tx)
+            haze[:,:,1] = img[:,:,1]*tx + A*255*(1-tx)
+            haze[:,:,2] = img[:,:,2]*tx + A*255*(1-tx)
+            # Guardamos imagen con niebla
+            img = Image.fromarray(np.clip(np.uint8(haze), 0, 255))
+            img.save(os.path.join(root_folder,img_dir,basedir,'haze',dirbeta,filename))
+print("Done!")
