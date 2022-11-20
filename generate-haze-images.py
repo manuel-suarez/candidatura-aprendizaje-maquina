48679f9828fffb338c87f622dedb0b2ed2a16b03
Arturo Suárez <manuel.suarez@cimat.mx>
2022-11-20 12:46:54 -0600

    Configuración de rutas

--- generate-haze-images.py
+++ generate-haze-images.py
@@ -5,3 +5,12 @@ from PIL import Image
 # from tqdm import tqdm
 import logging
 import matplotlib.pyplot as plt
+
+# Configuración de rutas
+# Ruta en el clúster
+root_folder = "/home/est_posgrado_manuel.suarez/data"
+img_dir = "ReDWeb-S"                # Base de datos
+base_dir = ["trainset", "testset"]
+clean_dir = "RGB"
+depth_dir = "depth"
+target_dir = "haze"
