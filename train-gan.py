f91de7195f963111de3c8ffc653e3d9e970b4e89
Arturo Suárez <manuel.suarez@cimat.mx>
2022-11-20 20:31:23 -0600

    Configuración y verificación de archivos

--- train-gan.py
+++ train-gan.py
@@ -13,10 +13,23 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 
-# Definición de variables
+# Definición de parámetros
 INPUT_DIM     = (256,256,3) # Tamaño de entrada
 OUTPUT_CHANNELS = 3         # La salida es la imágen RGB limpia
 BATCH_SIZE    = 10
 R_LOSS_FACTOR = 10000
 EPOCHS        = 100
 INITIAL_EPOCH = 0
+
+# Definición y configuración de las rutas
+local_dir = '/home/est_posgrado_manuel.suarez/data/ReDWeb-S/trainset'
+xfiles  = glob(os.path.join(local_dir, 'RGB', '*.jpg'))
+yfiles  = glob(os.path.join(local_dir, 'haze', 'beta05', '*.png'))
+xfiles.sort()
+yfiles.sort()
+xfiles=np.array(xfiles)
+yfiles=np.array(yfiles)
+
+# Verificamos las primeras 5 entradas para identificar si corresponden al mismo escenario
+for xl, xr, y in zip(xl_files[:5], xr_files[:5], y_files[:5]):
+  print(xl, xr, y)
