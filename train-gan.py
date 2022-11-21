c7ce0e17926ba588d8732161fadb631279e540a0
Arturo Suárez <manuel.suarez@cimat.mx>
2022-11-20 20:45:59 -0600

    Corrección de la extensión de los archivos con haze

--- train-gan.py
+++ train-gan.py
@@ -24,7 +24,7 @@ INITIAL_EPOCH = 0
 # Definición y configuración de las rutas
 local_dir = '/home/est_posgrado_manuel.suarez/data/ReDWeb-S/trainset'
 xfiles  = glob(os.path.join(local_dir, 'RGB', '*.jpg'))
-yfiles  = glob(os.path.join(local_dir, 'haze', 'beta05', '*.png'))
+yfiles  = glob(os.path.join(local_dir, 'haze', 'beta05', '*.jpg'))
 xfiles.sort()
 yfiles.sort()
 xfiles=np.array(xfiles)
