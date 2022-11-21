c2943ddb30188ce8173ba542ea27723d524366e9
Arturo Suárez <manuel.suarez@cimat.mx>
2022-11-21 00:18:45 -0600

    Definición de los bloques down y upsample de la arquitectura de la red

--- train-gan.py
+++ train-gan.py
@@ -131,3 +131,61 @@ for x, y in train_xy.take(3):
 for x, y in test_xy.take(3):
     display_images("figura3.png", x, y, rows=3)
     break
+
+# Definición de la arquitectura de la red
+
+# Bloque downsample
+def downsample(filters, size, apply_batchnorm=True):
+    '''
+    Bloque de codificación (down-sampling)
+    '''
+    initializer = tf.random_normal_initializer(0., 0.02)
+
+    result = tf.keras.Sequential()
+    result.add(tf.keras.layers.Conv2D(filters           = filters,
+                                      kernel_size       = size,
+                                      strides           = 2,
+                                      padding           = 'same',
+                                      kernel_initializer= initializer,
+                                      use_bias          = False))
+
+    if apply_batchnorm:
+        result.add(tf.keras.layers.BatchNormalization())
+
+    result.add(tf.keras.layers.LeakyReLU())
+
+    return result
+
+# Aplicación downsample
+down_model  = downsample(3, 4)
+down_result = down_model(tf.expand_dims(x_img, 0))
+print(down_result.shape)
+
+# Bloque upsample
+def upsample(filters, size, apply_dropout=False):
+    '''
+    Bloque de decodicación (up-sampling)
+    '''
+    initializer = tf.random_normal_initializer(0., 0.02)
+
+    result = tf.keras.Sequential()
+    result.add(tf.keras.layers.Conv2DTranspose(filters           = filters,
+                                               kernel_size       = size,
+                                               strides           = 2,
+                                               padding           = 'same',
+                                               kernel_initializer= initializer,
+                                               use_bias          = False))
+
+    result.add(tf.keras.layers.BatchNormalization())
+
+    if apply_dropout:
+        result.add(tf.keras.layers.Dropout(0.5))
+
+    result.add(tf.keras.layers.ReLU())
+
+    return result
+
+# Aplicación upsample
+up_model = upsample(3, 4)
+up_result = up_model(down_result)
+print (up_result.shape)
